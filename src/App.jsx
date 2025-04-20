038af9cae879fd5f08cfaeb878df5fef8cec6cde
Jose <josefbautista94@gmail.com>
2025-04-20 17:56:19 -0400

    feat: added clean footer component to base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import '/index.css'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import "./index.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-    
         {/* Add other routes like /projects /about /contact later */}
       </Routes>
+      <Footer />
     </Router>
   );
 }
